958701604088c11ee885599996df77cd6aba23f0
Irina-Gorozhina <123706883+Irina-Gorozhina@users.noreply.github.com>
2023-10-14 20:16:03 +1000

    Add files via upload

---
+++ main.js
@@ -0,0 +1,25 @@
+function loadHeader() {
+  var xhr = new XMLHttpRequest();
+  xhr.open("GET", "header.html", true);
+  xhr.send();
+  xhr.onreadystatechange = function () {
+      if (xhr.readyState == 4 && xhr.status == 200) {
+          document.getElementById("header-container").innerHTML = xhr.responseText;
+      loadFooter();
+      }
+  };
+}
+
+//  Footer
+function loadFooter() {
+  var xhr = new XMLHttpRequest();
+  xhr.open("GET", "footer.html", true);
+  xhr.send();
+  xhr.onreadystatechange = function () {
+      if (xhr.readyState == 4 && xhr.status == 200) {
+          document.body.insertAdjacentHTML("beforeend", xhr.responseText);
+      }
+  };
+}
+
+loadHeader();
