776ad9b991d409cd8bc3f808ff2d5d63f51249ef
IRINA-PROBOOK\hp <gorozhina@outlook.com>
2023-10-14 20:51:03 +1000

    sdfsdfsdfsd

--- js/main.js
+++ js/main.js
@@ -1,5 +1,33 @@
-document.addEventListener("DOMContentLoaded", function() {
+
+function triggerBurger() {
     document.getElementById("burger").addEventListener("click", function() {
         document.querySelector("header").classList.toggle("open")
     })
-})
+  }
+
+function loadHeader() {
+    var xhr = new XMLHttpRequest();
+    xhr.open("GET", "header.html", true);
+    xhr.send();
+    xhr.onreadystatechange = function () {
+        if (xhr.readyState == 4 && xhr.status == 200) {
+            document.getElementById("header-container").innerHTML = xhr.responseText;
+            loadFooter();
+            triggerBurger();
+        }
+    };
+  }
+  
+  //  Footer
+  function loadFooter() {
+    var xhr = new XMLHttpRequest();
+    xhr.open("GET", "footer.html", true);
+    xhr.send();
+    xhr.onreadystatechange = function () {
+        if (xhr.readyState == 4 && xhr.status == 200) {
+            document.body.insertAdjacentHTML("beforeend", xhr.responseText);
+        }
+    };
+  }
+  
+  loadHeader();
